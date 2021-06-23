7b22f1315ad5a569fb1132b962db829545ccd558
Marcos Amorim <markstm10@gmail.com>
2021-06-23 01:22:18 -0300

    🛠 criado função pra copiar pro clipboard o codigo da sala e estilizado

--- src/pages/Rooms.tsx
+++ src/pages/Rooms.tsx
@@ -1,14 +1,24 @@
+import { useParams } from "react-router-dom";
+
 import logoImg from "../assets/images/logo.svg";
 import { Button } from "../components/Button";
+import { RoomCode } from "../components/RoomCode";
 
 import "../styles/room.scss";
+
+type RoomParams = {
+  id: string;
+};
+
 export function Room() {
+  const params = useParams<RoomParams>();
+
   return (
     <div id="page-room">
       <header>
         <div className="content">
           <img src={logoImg} alt="Letmeask" />
-          <div>codigo</div>
+          <RoomCode code={params.id} />
         </div>
       </header>
 
