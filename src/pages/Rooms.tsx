bba4dfa0d67711c1a2b16bed3cc3f406e8faff6d
Marcos Amorim <markstm10@gmail.com>
2021-06-23 02:24:48 -0300

    🛠 Criado função pra carregar as perguntas do firebase

--- src/pages/Rooms.tsx
+++ src/pages/Rooms.tsx
@@ -9,9 +9,35 @@ import { useAuth } from "../hooks/useAuth";
 import { database } from "../services/firebase";
 
 import "../styles/room.scss";
+import { useEffect } from "react";
+import { StringDecoder } from "string_decoder";
 
 type RoomParams = {
   id: string;
+};
+
+type FirebaseQuestions = Record<
+  string,
+  {
+    author: {
+      name: string;
+      avatar: StringDecoder;
+    };
+    content: string;
+    isAnswered: boolean;
+    isHighlighted: boolean;
+  }
+>;
+
+type Question = {
+  id: string;
+  author: {
+    name: string;
+    avatar: StringDecoder;
+  };
+  content: string;
+  isAnswered: boolean;
+  isHighlighted: boolean;
 };
 
 export function Room() {
@@ -19,8 +45,32 @@ export function Room() {
 
   const params = useParams<RoomParams>();
   const [newQuestion, setNewQuestion] = useState("");
-
+  const [questions, setQuestions] = useState<Question[]>([]);
+  const [title, setTitle] = useState("");
   const roomId = params.id;
+
+  useEffect(() => {
+    const roomRef = database.ref(`rooms/${roomId}`);
+
+    roomRef.once("value", (room) => {
+      const databaseRoom = room.val();
+      const firebaseQuestions: FirebaseQuestions = databaseRoom.questions ?? {};
+
+      const parsedQuestions = Object.entries(firebaseQuestions).map(
+        ([key, value]) => {
+          return {
+            id: key,
+            content: value.content,
+            author: value.author,
+            isHighlighted: value.isHighlighted,
+            isAnswered: value.isAnswered,
+          };
+        }
+      );
+      setTitle(databaseRoom.title);
+      setQuestions(parsedQuestions);
+    });
+  }, [roomId, questions]);
 
   async function handleSendQuestion(event: FormEvent) {
     event.preventDefault();
@@ -37,7 +87,7 @@ export function Room() {
         name: user.name,
         avatar: user.avatar,
       },
-      isHighLighted: false,
+      isHighlighted: false,
       isAnswered: false,
     };
 
@@ -57,8 +107,8 @@ export function Room() {
 
       <main>
         <div className="room-title">
-          <h1>Sala React</h1>
-          <span>4 perguntas </span>
+          <h1>Sala {title}</h1>
+          {questions.length > 0 && <span>{questions.length} pergunta(s) </span>}
         </div>
 
         <form onSubmit={handleSendQuestion}>
@@ -83,6 +133,7 @@ export function Room() {
             </Button>
           </div>
         </form>
+        {JSON.stringify(questions)}
       </main>
     </div>
   );
