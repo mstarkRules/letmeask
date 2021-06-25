e44d3adbad2b8e159d240f033bdb97bc77398528
Marcos Amorim <markstm10@gmail.com>
2021-06-24 21:36:52 -0300

    🛠 create page adminroom

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { AuthContextProvider } from "./contexts/AuthContext";
 import { Room } from "./pages/Rooms";
 import { ThemeContextProvider } from "./contexts/ThemeContext";
+import { AdminRoom } from "./pages/AdminRoom";
 
 function App() {
   return (
@@ -16,6 +17,8 @@ function App() {
             <Route path="/" exact component={Home} />
             <Route path="/rooms/new" component={NewRoom} />
             <Route path="/rooms/:id" component={Room} />
+
+            <Route path="/admin/rooms/:id" component={AdminRoom} />
           </Switch>
         </AuthContextProvider>
       </ThemeContextProvider>
---
+++ src/pages/AdminRoom.tsx
@@ -0,0 +1,90 @@
+import { useState } from "react";
+import { useHistory, useParams } from "react-router-dom";
+import { FormEvent } from "react";
+import { History } from "history";
+
+import logoImg from "../assets/images/logo.svg";
+import { Button } from "../components/Button";
+import { RoomCode } from "../components/RoomCode";
+import { Question } from "../components/Question/index";
+
+import { useRoom } from "../hooks/useRoom";
+import { useAuth } from "../hooks/useAuth";
+import { database } from "../services/firebase";
+
+import "../styles/room.scss";
+import { useEffect } from "react";
+
+type RoomParams = {
+  id: string;
+};
+
+export function AdminRoom() {
+  const { user } = useAuth();
+  const history = useHistory();
+  const params = useParams<RoomParams>();
+  const [newQuestion, setNewQuestion] = useState("");
+  const roomId = params.id;
+
+  const { title, questions } = useRoom(roomId);
+
+  useEffect(() => {
+    if (!user) {
+      history.push("/");
+    }
+  }, [user, history]);
+
+  async function handleSendQuestion(event: FormEvent) {
+    event.preventDefault();
+    if (newQuestion.trim() == "") {
+      return;
+    }
+    if (!user) {
+      throw new Error("You must be logged in");
+    }
+
+    const question = {
+      content: newQuestion,
+      author: {
+        name: user.name,
+        avatar: user.avatar,
+      },
+      isHighlighted: false,
+      isAnswered: false,
+    };
+
+    await database.ref(`rooms/${roomId}/questions`).push(question);
+
+    setNewQuestion("");
+  }
+
+  return (
+    <div id="page-room">
+      <header>
+        <div className="content">
+          <img src={logoImg} alt="Letmeask" />
+          <RoomCode code={roomId} />
+        </div>
+      </header>
+
+      <main>
+        <div className="room-title">
+          <h1>Sala {title}</h1>
+          {questions.length > 0 && <span>{questions.length} pergunta(s) </span>}
+        </div>
+
+        <div className="question-list">
+          {questions.map((question) => {
+            return (
+              <Question
+                key={question.id}
+                content={question.content}
+                author={question.author}
+              />
+            );
+          })}
+        </div>
+      </main>
+    </div>
+  );
+}
