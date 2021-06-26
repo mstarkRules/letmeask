a19065938d5081d2ea15ab959ea5cc5e4a0cf8d6
Marcos Amorim <markstm10@gmail.com>
2021-06-26 02:20:54 -0300

    ❗ install bootstrap

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,6 +13,8 @@ import { database } from "../services/firebase";
 import { useTheme } from "../hooks/useTheme";
 import { useEffect } from "react";
 
+import { Navbar, Nav } from "react-bootstrap";
+
 export function Home() {
   const history = useHistory();
   const { user, signInWithGoogle, handleSetLoading } = useAuth();
@@ -56,34 +58,63 @@ export function Home() {
   }
 
   return (
-    <div id="page-auth" className={theme}>
-      <aside>
-        <img
-          src={illustrationImg}
-          alt="Ilustração simbolizando perguntas e respostas"
-        />
-        <strong>Crie salas de Q&amp;A ao-vivo</strong>
-        <p>Tire as dúvidas da sua audiência em tempo real</p>
-      </aside>
-      <main>
-        <div className="main-content">
-          <img src={logoImg} alt="Letmeask" />
-          <button onClick={handleCreateRoom} className="create-room">
-            <img src={googleIconImg} alt="Logo do Google" />
-            Crie sua sala com o Google
-          </button>
-          <div className="separator">ou entre em uma sala</div>
-          <form onSubmit={handleJoinRoom}>
-            <input
-              type=""
-              placeholder="Digite o código da sala"
-              onChange={(event) => setRoomCode(event.target.value)}
-              value={roomCode}
-            />
-            <Button type="submit">Entrar na sala</Button>
-          </form>
-        </div>
-      </main>
-    </div>
+    <>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">More deets</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              Dank memes
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <div id="page-auth" className={theme}>
+        <aside>
+          <img
+            src={illustrationImg}
+            alt="Ilustração simbolizando perguntas e respostas"
+          />
+          <strong>Crie salas de Q&amp;A ao-vivo</strong>
+          <p>Tire as dúvidas da sua audiência em tempo real</p>
+        </aside>
+        <main>
+          <div className="main-content">
+            <img src={logoImg} alt="Letmeask" />
+            <button onClick={handleCreateRoom} className="create-room">
+              <img src={googleIconImg} alt="Logo do Google" />
+              Crie sua sala com o Google
+            </button>
+            <div className="separator">ou entre em uma sala</div>
+            <form onSubmit={handleJoinRoom}>
+              <input
+                type=""
+                placeholder="Digite o código da sala"
+                onChange={(event) => setRoomCode(event.target.value)}
+                value={roomCode}
+              />
+              <Button type="submit">Entrar na sala</Button>
+            </form>
+          </div>
+        </main>
+      </div>
+    </>
   );
 }
